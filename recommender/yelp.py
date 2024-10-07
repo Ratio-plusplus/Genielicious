bd7152578db5ae4c2be2d1ad91f898374be8abcf
Alex <alexog404@gmail.com>
2024-10-07 13:30:04 -0700

    unincluded old api key

--- recommender/yelp.py
+++ recommender/yelp.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-# # # DO NOT share to public. These API details must be deactivated if this repo were to ever go public.
-ID = "3lMv7jkZTdziX-utSK_Ngw"
-API_Key = "8Ncp-HYEUfTSg3Y8FzCP1MYj3n3SPxEGxJgJcV5fw2hNAKtr8COMJ2hVYP5i_7Ef8GGvtz6TzvlHPG9DZVOqM9BDD3bb6xXFPFEJd0suUL2J7VOM5WehSTl1cVgDZ3Yx"
+# # # DO NOT share to public. These API details must be deactivated if this repo were public.
+ID = "id"
+API_Key = "api key"
 # # #
 
 CSULB_coordinates = (33.78336745904146, -118.1101659429386) # test location (lat,long)
