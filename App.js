2665882020b9daff380a0ccbcb3b3d8c946a8e25
kickingdude <kameronferrer8@gmail.com>
2024-10-07 19:39:19 -0700

    App.js and Firebase added

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import WelcomeContainer from './navigation/WelcomeContainer';
+import { initializeApp } from 'firebase/app';
+
 
 function App() {
+    const app = initializeApp(firebaseConfig);
   return(
     <WelcomeContainer/>
   );
