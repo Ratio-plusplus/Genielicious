b83ffe4e512eda67f141577dd141ad6f06fe2287
Alex <alexog404@gmail.com>
2024-10-18 04:10:19 -0700

    added answer processing to backend call

--- backend/recommender/decider.py
+++ backend/recommender/decider.py
@@ -4,13 +4,13 @@ import json
 import os
 from yelp import getStore
 
-SUPPORTED_REGIONAL_CUISINES = set(["African", "Middle Eastern", "South Asian", # cultural categories that are currently supported
-                                   "East Asian", "European", "Latin American", 
-                                   "North American"])
-ALL_OTHER_CATEGORIES = set(["Finger food", "Specialty", "Light Meals", "Vegan", 
-                            "Quick Eats", "Breakfast", "Desserts", "Health-Conscious",
-                            "Meat-Centric", "Comfort Food"])
-MAIN_FLAVORS = set(["sweet","salty", "sour", "umami", "spicy"]) # main flavors that we will take into account
+SUPPORTED_REGIONAL_CUISINES = {"7":"African", "8":"Middle Eastern", "9":"South Asian", # cultural categories that are currently supported
+                                "10":"East Asian", "11":"European", "12":"Latin American", 
+                                "13":"North American"}
+# ALL_OTHER_CATEGORIES = set(["Finger food", "Specialty", "Light Meals", "Vegan", 
+#                             "Quick Eats", "Breakfast", "Desserts", "Health-Conscious",
+#                             "Meat-Centric", "Comfort Food"])
+MAIN_FLAVORS = {"2":"sweet","3":"salty", "4":"sour", "5":"umami", "6":"spicy"} # main flavors that we will take into account
 
 with open("data\\categorized_aliases.json", "r") as file:
     CATEGORIZED_ALIASES = json.load(file)
@@ -45,10 +45,10 @@ def getDistance(response:dict, sid:int) -> None:
     response["total"] += 1
 
 # get desired flavors in form of space delimited string
-def getFlavors(response:dict, sid:int) -> None:
-    for i,flavor in enumerate(MAIN_FLAVORS):
+def getFlavors(response:dict) -> None:
+    for sid,flavor in MAIN_FLAVORS.items():
         response["questions"].append({
-            "id": str(sid+i),
+            "id": sid,
             "question": f"Do you want something {flavor}?",
             "answer_map": {
                 "yes": 1, 
@@ -59,10 +59,10 @@ def getFlavors(response:dict, sid:int) -> None:
         response["total"] += 1
 
 # gets desired food categories in form of comma delimited string
-def getCulture(response:dict, sid:int) -> None:
-    for i,culture in enumerate(SUPPORTED_REGIONAL_CUISINES):
+def getCulture(response:dict) -> None:
+    for sid,culture in SUPPORTED_REGIONAL_CUISINES.items():
         response["questions"].append({
-            "id": str(sid+i),
+            "id": sid,
             "question": f"Would you want {culture} food?",
             "answer_map": {
                 "yes": 1, 
@@ -72,17 +72,58 @@ def getCulture(response:dict, sid:int) -> None:
         })
         response["total"] += 1
 
+# compiles questions to provide to frontend user; id's for each question provided
 def getShortSessionQuestions(hasDistance:bool = False, hasPrice:bool = False) -> str:
     response = {"questions": [], "total" : 0}
     if not hasPrice: 
         getPriceQuestion(response, 0) # adds question to response object
     if not hasDistance:
-        getDistance(response, 5)
+        getDistance(response, 1)
 
-    getFlavors(response, 9)
-    getCulture(response, 20)
+    getFlavors(response)
+    getCulture(response)
 
     return response
+
+def processShortSessionAnswers(answers):
+    # TODO: CHANGE HARDCODED LOCATION TO USER'S LOCATION BASED OFF DB
+    USER_LOCATION = (33.78336745904146, -118.1101659429386)
+    term = ""
+    temp_term = ""
+    categories = ""
+    category_temp = ""
+
+    price = int(answers["0"])
+    distance = int(answers["1"])
+
+    for i in range(2,2+len(MAIN_FLAVORS)):
+        qid = str(i)
+        if answers[qid] == "1": # 1 = yes
+            term += MAIN_FLAVORS[qid] + " "
+        elif answers[qid] == "2": # 2 = maybe
+            temp_term = MAIN_FLAVORS[qid]
+    if not term: # empty string, no yes answer, give the maybes
+        term = temp_term
+
+    for j in range(2+len(MAIN_FLAVORS),len(SUPPORTED_REGIONAL_CUISINES)):
+        qid = str(j)
+        cur_culture = SUPPORTED_REGIONAL_CUISINES[qid]
+        if answers[qid] == "1": # 1 = yes
+            categories = ",".join(CATEGORIZED_ALIASES[cur_culture]) # gets all categories from supported culture
+            break
+        elif answers[qid] == "2": # 2 = maybe; cache possible category
+            category_temp = cur_culture
+
+    if not categories and category_temp: # assign the maybe categories if empty
+        categories= ",".join(CATEGORIZED_ALIASES[category_temp])    
+    elif not categories: # if maybe categories still empy then assign broad category
+        categories = "food,restaurants"
+
+    results = getStore(USER_LOCATION, term = term, categories=categories, price = price, radius = distance)
+    if results["total"] == 0: # check if there are any results
+        results = getStore(USER_LOCATION, term = term.split(" ")[0], categories="food,restaurants", price = price, radius = distance)
+
+    return results
 
 if __name__ == "__main__":
     from yelp import cacheToJson
--- backend/recommender/server.py
+++ backend/recommender/server.py
@@ -1,6 +1,6 @@
 # testing Flask
-from flask import Flask, redirect, url_for, request
-# import json
+from flask import Flask, redirect, url_for, request, abort
+import json
 import uuid
 import decider
 
@@ -11,18 +11,33 @@ app = Flask(__name__)
 def sendShortQuestions():
     query = request.args
 
-    distance_val = query.get("hasDistance",False) # should be boolean, decides if the price/distance should be provided; 
-    hasDistance = distance_val.lower() == "true"  # ... do not provide in event where distance and price pref are already known
-    price_str = query.get("hasPrice", False)
-    hasPrice = price_str == "true"
+    # booleans that decides if the price/distance question should be provided;
+    # ... do not provide in event where distance and price pref are already known
+    distance_bool = query.get("hasDistance", "false")
+    hasDistance = distance_bool.lower() == "true"  
+    
+    price_bool = query.get("hasPrice", "false")
+    hasPrice = price_bool.lower() == "true"
     
     return decider.getShortSessionQuestions(hasPrice=hasPrice,hasDistance=hasDistance)
 
 # receives short question answers to process results and return yelp api results
 @app.route("/client/answers/short")
 def receiveShortAnswers():
-    # TODO
-    pass
+    # TODO: verify answers are valid
+    query = request.args
+    answers = query.get("answers",None)
+    # answers should be in this format:
+    # query["answers"] = 
+    #               {
+    #                   "<questionID>": "<answer>",
+    #                    ...
+    #               }
+    if not answers:
+        abort(400, "Parameter \"answers\" not provided.")
+    
+    answers = json.loads(answers)
+    return decider.processShortSessionAnswers(answers)
 
 if __name__ == "__main__":
     app.run(debug=True)
