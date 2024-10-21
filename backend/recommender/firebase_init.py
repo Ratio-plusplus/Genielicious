0725a85ff3d48096904101d62b1dec988bcfde07
Alex <alexog404@gmail.com>
2024-10-21 10:21:44 -0700

    integrating firebase to access user information + more work on gemini api

--- backend/recommender/firebase_init.py
+++ backend/recommender/firebase_init.py
@@ -1,18 +1,29 @@
 # single script to handle connection/auth to database
 # !sudo pip install firebase-admin
-from firebase_admin import auth, db, credentials, initialize_app
+from firebase_admin import db, credentials, initialize_app
 from dotenv import find_dotenv, load_dotenv
 import os
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path) # loads env vars into path
 
-cred = credentials.Certificate("Genielicious\\backend\\recommender\\confidential\\serviceAccountKey.json")
+cred = credentials.Certificate("backend\\recommender\\confidential\\serviceAccountKey.json")
 db_url = {'databaseURL': os.getenv("DATABASE_URL")}
 
 genie_app = initialize_app(cred, db_url)
-print(genie_app.name)  # "[DEFAULT]"
-users = db.reference("users/9jdHehIN0JZP4UEiTSryshhxUJj1/flavorProfile/Image").get()
-home = db.reference("/fake_keys/-O9bjjOeoP7w-665SWwp/")
-home.set("another attribute to delete")
-print(home.get())
+
+def getTestUser(user_id):
+    return db.reference(f"test_users/{user_id}")
+
+def getUser(user_id):
+    return db.reference(f"users/{user_id}")
+# def changeID(this, that):
+#     user = db.reference(f"test_users/{this}")
+#     test_keys = db.reference(f"test_users/")
+
+#     old_data = user.get()
+
+#     test_keys.update({
+#         that:old_data
+#     })
+#     user.delete()
--- backend/recommender/gemini.py
+++ backend/recommender/gemini.py
@@ -1,41 +1,62 @@
 #!pip install -q -U google-generativeai
-import requests
+# import requests
 import json
-import os
+from firebase_init import getTestUser
 from dotenv import find_dotenv, load_dotenv
+from yelp import cacheToJson
 import google.generativeai as genai
 import os
-from yelp import cacheToJson
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 API_KEY = os.getenv("GEMINI_API_KEY")
-
 genai.configure(api_key=API_KEY)
 
-history = [
-        {"role":"user",
-         "parts":[{
-           "text": "Hello"}]},
-        {"role": "model",
-         "parts":[{
-           "text": "Great to meet you. What would you like to know?"}]},
-        {"role":"user",
-         "parts":[{
-           "text": "I have two dogs in my house. How many paws are in my house?"}]},
-      ]
-    
-
 model = genai.GenerativeModel("gemini-1.5-flash") # model version we're using
-response = model.generate_content( 
-    history, # "contents" parameter
-    generation_config=genai.types.GenerationConfig(        
-        candidate_count=1, # Only one candidate option for now.
-        # stop_sequences=["x"],
-        # max_output_tokens=20,
-        temperature=1.0,
-    ),
-)
 
-cacheToJson("backend\\recommender\\tests\\model_history.json",response.text)
-print(response.text)
+def formatStringToJson(s):
+  data = str(s).replace('json', '').replace('\\n', '').replace('\\"', '"').replace('```', '').replace('\n', '').replace('\\', '')
+  return json.loads(json.dumps(data)) # returns json dictionary as a string
+
+def submitAnswer(user_id,answer:str):
+  #TODO: Reference cache to send to model
+  pass
+
+def getNextQuestion(user_id:str):
+  user = getTestUser(user_id) ### TODO: change to actual users when complete
+
+  surveyCache_ref = user.child("surveyCache")
+  surveyCache = surveyCache_ref.get()
+
+  if not surveyCache:
+    with open("backend\\recommender\\tests\\default_prompt.txt", "r") as file:
+      default_prompt = file.readline()
+    surveyCache = [
+          {"role":"user",
+          "parts":[{
+            "text": default_prompt}]},
+        ]
+  else:
+    surveyCache = json.loads(surveyCache_ref)
+    if surveyCache[-1]["role"] == "model":
+      return { "error" : "Expecting client answer to model question."}
+
+  response = model.generate_content(
+      surveyCache, # "contents" parameter
+      generation_config=genai.types.GenerationConfig(
+          temperature=1.0, # "randomness" of model
+      ),
+  )
+  parts = str(response.parts)
+  surveyCache.append({"role":"model","parts":parts})
+  surveyCache = json.dumps(surveyCache) # saves cache as a single string
+
+
+  user.update({"surveyCache" : surveyCache})
+  formatted_question = formatStringToJson(response.text)
+  cacheToJson("backend\\recommender\\tests\\model_history.json",surveyCache)
+  return formatted_question
+
+if __name__ == "__main__":
+  getNextQuestion("3")
+  submitAnswer("3", "yes")
