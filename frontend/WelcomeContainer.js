692aa24cdb73e88e9f70341c4e4c99ae31c05e2f
Kyla Do <kd.katiedo3@gmail.com>
2024-10-21 22:00:47 -0700

    added images assets, home, question, answer, and result screens

--- frontend/WelcomeContainer.js
+++ frontend/WelcomeContainer.js
@@ -10,6 +10,9 @@ import Settings from './screens/Settings';
 import EditProfile from './screens/EditProfile';
 import AddPref1 from './screens/AddPref1';
 import AddPref2 from './screens/AddPref2';
+import Question from './screens/Question';
+import Answer from './screens/Answer';
+import Result from './screens/Result';
 
 const loginName = 'Login';
 const signupName = 'Signup';
@@ -18,6 +21,9 @@ const settingsName = 'Settings';
 const editName = 'Edit Profile';
 const add1Name = "Add Preference 1";
 const add2Name = "Add Preference 2";
+const questionName = "Question";
+const answerName = "Answer";
+const resultName = "Result";
 
 const Stack = createNativeStackNavigator();
 
@@ -33,6 +39,9 @@ export default function WelcomeContainer() {
         <Stack.Screen name={editName} component={EditProfile}/>
         <Stack.Screen name={add1Name} component={AddPref1}/>
         <Stack.Screen name={add2Name} component={AddPref2}/>
+        <Stack.Screen name={questionName} component={Question}/>
+        <Stack.Screen name={answerName} component={Answer}/>
+        <Stack.Screen name={resultName} component={Result}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
