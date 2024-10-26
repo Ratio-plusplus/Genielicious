ae0ebb8bc8d8e4544648ef1db4d650f800caa86c
Alex <alexog404@gmail.com>
2024-10-26 01:56:04 -0700

    fully tested budget/distance question functionality

--- backend/recommender/gemini.py
+++ backend/recommender/gemini.py
@@ -42,33 +42,32 @@ def submitAnswer(user_id,answer):
     # Checks if distance/budget needs to be updated
     if "distance" in surveyCache:
       if answer == "10 miles or less":
-        user.update({"distance": 16093}) # store distance in meters as an integer
+        user.update({"distanceCache": 16093}) # store distance in meters as an integer
       elif answer == "15 miles or less":
-        user.update({"distance": 24140})
+        user.update({"distanceCache": 24140})
       elif answer == "20 miles or less": 
-        user.update({"distance": 32187})
+        user.update({"distanceCache": 32187})
       elif answer == "25 miles or less":
-        user.update({"distance": 40000})
+        user.update({"distanceCache": 40000})
       else:
         return {"Error 401" : "Invalid answer."}
       return {"success": True, "results": False}
     elif "budget" in surveyCache:
       if answer == "$10 or less":
-        user.update({"budget": 1}) # price intervals according to yelp api
+        user.update({"budgetCache": 1}) # price intervals according to yelp api
       elif answer == "$30 or less":
-        user.update({"budget": 2})
+        user.update({"budgetCache": 2})
       elif answer == "$60 or less": 
-        user.update({"budget": 3})
+        user.update({"budgetCache": 3})
       elif answer == "More than $60":
-        user.update({"budget": 4})
+        user.update({"budgetCache": 4})
       else:
         return {"Error 401" : "Invalid answer."}
-      user.update({"surveryCache":""})
+      user.update({"surveyCache":"",})
       return {"success": True, "results": False}
     elif surveyCache[-1]["role"] == "user": # if not user then the last response was model
       return {"Error 401" : "Client already answered question."}
     elif "recommendations" in json.loads(surveyCache[-1]["parts"]): # checks if last response from model were the recommendations
-      results.clearCache(user_id) # erase surveyCache and resultsCache before compiling results
       results.compileResults(user_id, answer) # puts results into resultsCache
       return {"success": True, "results": True}
 
@@ -85,6 +84,22 @@ def getNextQuestion(user_id:str, mode:str):
   user = getUser(user_id)
 
   surveyCache = user.child("surveyCache").get()
+  # TODO: update distance/price questions once active profiles are set (next sprint)
+  # provide distance/budget question if preference does not exist
+  distance = user.child("distance").get()
+  distanceCache = user.child("distanceCache").get()
+  budget = user.child("budget").get()
+  budgetCache = user.child("budgetCache").get()
+  print(not distance, not distanceCache)
+  print(not budget, not budgetCache)
+  if not distance and not distanceCache:
+    distance_question = {"question": "How far are you willing to travel?","answers":["10 miles or less", "15 miles or less", "20 miles or less", "25 miles or less"]}
+    user.update({"surveyCache":json.dumps({"distance":"need distance"})})
+    return distance_question
+  if not budget and not budgetCache:
+    budget_question = {"question": "How much money are you willing to spend?","answers":["$10 or less", "$30 or less", "$60 or less", "More than $60"]}
+    user.update({"surveyCache":json.dumps({"budget":"need budget"})})
+    return budget_question
 
   if not surveyCache: # empty survey cache = new session
     # Default prompt may differ based on user/mode
@@ -98,18 +113,6 @@ def getNextQuestion(user_id:str, mode:str):
       # last response in the chat was the model; thus expecting user response through submitAnswer()
       return {"Error 401" : "Model has already provided question OR result"}
 
-  # TODO: update distance/price questions once active profiles are set (next sprint)
-  # provide distance/budget question if preference does not exist
-  distance = user.child("distance").get()
-  budget = user.child("budget").get()
-  if not distance:
-    distance_question = {"question": "How far are you willing to travel?","answers":["10 miles or less", "15 miles or less", "20 miles or less", "25 miles or less"]}
-    user.update({"surveyCache":json.dumps({"distance":"need distance"})})
-    return distance_question
-  if not budget:
-    budget_question = {"question": "How much money are you willing to spend?","answers":["$10 or less", "$30 or less", "$60 or less", "More than $60"]}
-    user.update({"surveyCache":json.dumps({"budget":"need budget"})})
-    return budget_question
     
   response = model.generate_content(
       surveyCache, # "contents" parameter
--- backend/recommender/results.py
+++ backend/recommender/results.py
@@ -1,19 +1,24 @@
-from firebase_init import getTestUser 
+# from firebase_init import getTestUser 
 from firebase_init import getUser
 from yelp import getStore
 import json
 
 def clearCache(user_id):
-    user = getTestUser(user_id)
+    # user = getTestUser(user_id)
+    user = getUser(user_id)
     user.update({
         "surveyCache" : "",
-        "resultsCache" : ""
+        "budgetCache" : "",
+        "resultsCache": "",
+        "distanceCache": ""
     })
 
     return f"Successfullly cleared cache from userID: {user_id}" # confirmation
 
 def getResults(user_id):
-    user = getTestUser(user_id) # TODO: change to getUser()
+    # user = getTestUser(user_id) 
+    user = getUser(user_id)
+    
     results = user.child("resultsCache").get()
     if not results:
         return {"Error 401" : "No results compiled."}
@@ -21,34 +26,33 @@ def getResults(user_id):
     return results
 
 def compileResults(user_id, food_item:str):
-    user = getTestUser(user_id) # TODO: change to getUser()
+    # user = getTestUser(user_id) 
+    user = getUser(user_id)
 
     # getting user location
     latitude = user.child("location").child("latitude").get()
     longitude = user.child("location").child("longitude").get()
     coords = (latitude, longitude)
 
+    #TODO: update how distance and budget are retrieved to reflect how active food profiles are setup
     # getting user budget
     budget = user.child("budget").get()
     if budget: # checks if budget exists and is not empty string
         budget = int(budget)
         if budget > 4:
             budget = 4
-    else: budget = None
+    else: budget = int(user.child("budgetCache").get())
 
-    #TODO: update how distance and budget are retrieved to reflect how active food profiles are setup
     # getting user distance preference
     distance = user.child("distance").get()
     if distance: # checks if distance exists and is not empty string
         distance = int(distance)
-    else: distance = None
+    else: distance = int(user.child("distanceCache").get())
 
     yelp_results = getStore(coords, term = food_item, price=budget, radius=distance)
     # [OPTIONAL]: Parse Yelp results to not include unnecessary/extra fields
 
+    clearCache(user_id)
     user.update({
         "resultsCache" : json.dumps(yelp_results)
     })
-    # user.update({ #### TESTING
-    #     "resultsCache" : json.dumps({"Test output" : "this is test"})
-    # })
