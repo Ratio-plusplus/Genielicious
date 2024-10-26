184c69a1798c982e6b68e0c00fbec7e437b841f5
Alex <alexog404@gmail.com>
2024-10-26 00:58:04 -0700

    added budget/distance questions to recommender

--- backend/recommender/gemini.py
+++ backend/recommender/gemini.py
@@ -38,8 +38,34 @@ def submitAnswer(user_id,answer):
     return {"Error 401" : "No question provided yet."} 
   else:
     surveyCache = json.loads(surveyCache)
-    # TODO: check if the answer corresponds to budget/distance (next sprint)
-    if surveyCache[-1]["role"] == "user": # if not user then the last response was model
+    # TODO: update section after active profiles are set (next sprint)
+    # Checks if distance/budget needs to be updated
+    if "distance" in surveyCache:
+      if answer == "10 miles or less":
+        user.update({"distance": 16093}) # store distance in meters as an integer
+      elif answer == "15 miles or less":
+        user.update({"distance": 24140})
+      elif answer == "20 miles or less": 
+        user.update({"distance": 32187})
+      elif answer == "25 miles or less":
+        user.update({"distance": 40000})
+      else:
+        return {"Error 401" : "Invalid answer."}
+      return {"success": True, "results": False}
+    elif "budget" in surveyCache:
+      if answer == "$10 or less":
+        user.update({"budget": 1}) # price intervals according to yelp api
+      elif answer == "$30 or less":
+        user.update({"budget": 2})
+      elif answer == "$60 or less": 
+        user.update({"budget": 3})
+      elif answer == "More than $60":
+        user.update({"budget": 4})
+      else:
+        return {"Error 401" : "Invalid answer."}
+      user.update({"surveryCache":""})
+      return {"success": True, "results": False}
+    elif surveyCache[-1]["role"] == "user": # if not user then the last response was model
       return {"Error 401" : "Client already answered question."}
     elif "recommendations" in json.loads(surveyCache[-1]["parts"]): # checks if last response from model were the recommendations
       results.clearCache(user_id) # erase surveyCache and resultsCache before compiling results
@@ -55,7 +81,6 @@ def submitAnswer(user_id,answer):
   
 
 def getNextQuestion(user_id:str, mode:str):
-  # TODO: check if distance/price exists in active food profile (next sprint)
   # user = getTestUser(user_id) 
   user = getUser(user_id)
 
@@ -72,6 +97,19 @@ def getNextQuestion(user_id:str, mode:str):
     if surveyCache[-1]["role"] == "model": 
       # last response in the chat was the model; thus expecting user response through submitAnswer()
       return {"Error 401" : "Model has already provided question OR result"}
+
+  # TODO: update distance/price questions once active profiles are set (next sprint)
+  # provide distance/budget question if preference does not exist
+  distance = user.child("distance").get()
+  budget = user.child("budget").get()
+  if not distance:
+    distance_question = {"question": "How far are you willing to travel?","answers":["10 miles or less", "15 miles or less", "20 miles or less", "25 miles or less"]}
+    user.update({"surveyCache":json.dumps({"distance":"need distance"})})
+    return distance_question
+  if not budget:
+    budget_question = {"question": "How much money are you willing to spend?","answers":["$10 or less", "$30 or less", "$60 or less", "More than $60"]}
+    user.update({"surveyCache":json.dumps({"budget":"need budget"})})
+    return budget_question
     
   response = model.generate_content(
       surveyCache, # "contents" parameter
