1098b089f689b94943403cd30e971b6afa77d53f
Alex <alexog404@gmail.com>
2024-11-17 21:20:32 -0800

    fine tuning ai

--- backend/recommender/gemini.py
+++ backend/recommender/gemini.py
@@ -174,15 +174,19 @@ if __name__ == "__main__":
   with open(r"backend\recommender\unused\survey.json", "r") as file:
     prompt = json.load(file)["prompt"] 
 
-  surveyCache = [{"role":"user","parts":[{"text": prompt}]},]
-  response = model.generate_content(
-      surveyCache, # "contents" parameter
-      generation_config=genai.types.GenerationConfig(
-          temperature=1.0, # "randomness" of model
-      ),
-  )
-  cacheToJson(r"backend\recommender\unused\res.json",response.text)
-  cacheToJson(r"backend\recommender\unused\res_out.json",formatStringToJson(response.text))
+  surveyCache = [{"role":"user","parts":[{"text": prompt}]}]
+  while True:
+    response = model.generate_content(
+        surveyCache, # "contents" parameter
+        generation_config=genai.types.GenerationConfig(
+            temperature=1.0, # "randomness" of model
+        ),
+    )
+    surveyCache.append({"role":"model","parts":response.text})
+    # cacheToJson(r"backend\recommender\unused\res.json",response.text)
+    cacheToJson(r"backend\recommender\unused\res_out.json",formatStringToJson(response.text))
+    prompt = input("Response: ")
+    surveyCache.append({"role":"user", "parts":prompt})
 
   # print(getNextQuestion("3"))
   # submitAnswer("3", "yes")
