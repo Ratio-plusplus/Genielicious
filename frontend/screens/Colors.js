b3460a0f52bbc0c4ad8a11b90444e7c9d814c6b6
Alex <alexog404@gmail.com>
2024-11-27 15:23:23 -0800

    dedicated backend folder

--- frontend/screens/Colors.js
+++
@@ -1,9 +0,0 @@
-export const Colors = {
-    blue: "#2C3E50",
-    champagne: "#F4D1AE",
-    ghost: "#EBEEFA",
-    gold: "#ED9A1C", 
-    darkGold: "#BF7D19",
-    yellow: "#F0C016",
-    raisin: "#272725"
-};
