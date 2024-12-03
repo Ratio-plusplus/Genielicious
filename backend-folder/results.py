d33fad10df4cb6de1af2d7abba54bba11c3deeb5
Alex <alexog404@gmail.com>
2024-12-02 23:53:06 -0800

    fix for result code

--- backend-folder/results.py
+++ backend-folder/results.py
@@ -50,14 +50,14 @@ def compileResults(user_id, food_item:str):
         cache.update({"resultsCache" : json.dumps(error)})
         return error
 
-    total = yelp_results.get("businesses",0)
+    total = len(yelp_results.get("businesses",[]))
 
     # handle when no businesses are given
     if total == 0:
         status = 2
         # broaden search parameters a bit
         yelp_results = getStore(coords, term = food_item.split(" ")[0:2], categories="food",price=budget, radius=distance)
-        total = yelp_results.get("businesses",0)
+        total = len(yelp_results.get("businesses",[]))
 
     # check if there are any new results
     if total == 0:
